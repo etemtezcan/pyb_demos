4a9a7fd164716e6424a31027840622fc7a159cd3
etemtezcan <etem.tezcan@gmail.com>
2014-08-13 16:58:53 +0300

    saves to the file

--- save_deneme.py
+++ save_deneme.py
@@ -1,7 +1,5 @@
-
 import pyb
 import sys
-#sys.path.append('1:/') #to find modules in sdCard 
 from pyb import I2C
 from struct import unpack as unp
 from MPU_6050 import MPU_6050
@@ -34,17 +32,16 @@ while True:
         t = pyb.millis()
         while (i < 1000):
             x, y, z = mpu.get_acc_values()
+            t = pyb.millis()
             acc_x.append(x)
             acc_y.append(y)
             acc_z.append(z)
             tim.append(t)
             pyb.delay(1)
             i += 1
+        while i>-1:
+            i -= 1
+            vib_data.write('{},{},{},{}\n'.format(tim[i],acc_x[i],acc_y[i],acc_z[i]))
         vib_data.close() #after the button is pressed again, close the logging file
         light.off() #switch off the light in order to indicate that the logging has stopped
         pyb.delay(200) #wait for button press
-        
-        #i = 10    
-        #while i>0:
-        #    i -= 1  
-        #    vib_data.write('{},{},{},{}\n'.format(tim[1],acc_x[1],acc_y[1],acc_z[1]))  
